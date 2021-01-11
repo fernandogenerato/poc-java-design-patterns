27903332b77f8664284bb8bd44f83a30b17c94b0
Fernando <generatof@gmail.com>
2021-01-11 02:23:17 -0300

    commit

--- src/br/com/design/patterns/Main.java
+++ src/br/com/design/patterns/Main.java
@@ -1,8 +1,18 @@
 package br.com.design.patterns;
 
+import br.com.design.patterns.imposto.CalculadoraDeImpostos;
+import br.com.design.patterns.imposto.TipoImposto;
+import br.com.design.patterns.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+        System.out.println(calculadora.calcular(orcamento, TipoImposto.ICMS));
+
     }
 }
---
+++ src/br/com/design/patterns/imposto/TipoImposto.java
@@ -0,0 +1,5 @@
+package br.com.design.patterns.imposto;
+
+public enum TipoImposto {
+    ICMS,ISS;
+}
