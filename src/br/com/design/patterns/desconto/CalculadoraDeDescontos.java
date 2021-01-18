287cd56834ebbbaa9d09f7fa63ab2a13036f56f0
Fernando <generatof@gmail.com>
2021-01-18 01:04:06 -0300

    Template Method

--- src/br/com/design/patterns/desconto/CalculadoraDeDescontos.java
+++ src/br/com/design/patterns/desconto/CalculadoraDeDescontos.java
@@ -4,17 +4,14 @@ import br.com.design.patterns.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
+
 public class CalculadoraDeDescontos {
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		Desconto desconto = new DescontoParaOrcamentoComMaisDeCincoItens(
+				new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
+						new SemDesconto()));
+		return desconto.calcular(orcamento);
+	}
 
-    public BigDecimal calcular(Orcamento orcamento) {
-
-        Desconto desconto =
-                new DescontoParaOrcamentoComMaisDeCincoItens(
-                new DescontoParaOrcamentoComValorMaiorQueQuinhentos(
-                new SemDesconto())
-
-        );
-
-        return desconto.calcular(orcamento);
-    }
 }
--- src/br/com/design/patterns/desconto/DescontoParaOrcamentoComValorMaiorQueQuinhentos.java
+++ src/br/com/design/patterns/desconto/DescontoParaOrcamentoComValorMaiorQueQuinhentos.java
@@ -4,17 +4,21 @@ import br.com.design.patterns.orcamento.Orcamento;
 
 import java.math.BigDecimal;
 
+
 public class DescontoParaOrcamentoComValorMaiorQueQuinhentos extends Desconto {
+
     public DescontoParaOrcamentoComValorMaiorQueQuinhentos(Desconto proximo) {
         super(proximo);
     }
 
-    public BigDecimal calcular(Orcamento orcamento) {
-
-        if (orcamento.getValor().compareTo(new BigDecimal("500")) > 0) {
-            return orcamento.getValor().multiply(new BigDecimal("0.05"));
-
-        }
-        return proximo.calcular(orcamento);
+    @Override
+    public BigDecimal efetuarCalculo(Orcamento orcamento) {
+        return orcamento.getValor().multiply(new BigDecimal("0.05"));
     }
+
+    @Override
+    public boolean deveAplicar(Orcamento orcamento) {
+        return orcamento.getValor().compareTo(new BigDecimal("500")) > 0;
+    }
+
 }
