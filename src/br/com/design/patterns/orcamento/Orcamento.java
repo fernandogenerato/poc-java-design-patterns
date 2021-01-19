e0a8a63511aea3e9f61e7eebc2b070a9b6eff84b
Fernando <generatof@gmail.com>
2021-01-19 02:28:20 -0300

    Patter State

--- src/br/com/design/patterns/orcamento/Orcamento.java
+++ src/br/com/design/patterns/orcamento/Orcamento.java
@@ -4,20 +4,37 @@ import java.math.BigDecimal;
 
 public class Orcamento {
 
-	private BigDecimal valor;
-	private int quantidadeItens;
+    private BigDecimal valor;
+    private int quantidadeItens;
+    private String situacao;
 
-	public Orcamento(BigDecimal valor, int quantidadeItens) {
-		this.valor = valor;
-		this.quantidadeItens = quantidadeItens;
-	}
+    public Orcamento(BigDecimal valor, int quantidadeItens) {
+        this.valor = valor;
+        this.quantidadeItens = quantidadeItens;
+    }
 
-	public BigDecimal getValor() {
-		return valor;
-	}
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
 
-	public int getQuantidadeItens() {
-		return quantidadeItens;
-	}
+        if (situacao.equals("EM ANALISE")) {
+            valorDoDescontoExtra = new BigDecimal("0.05");
+        } else if (situacao.equals("APROVADO")
+        ) {
+            valorDoDescontoExtra = new BigDecimal("0.02");
+        }
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao = "APROVADO";
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public int getQuantidadeItens() {
+        return quantidadeItens;
+    }
 
 }
