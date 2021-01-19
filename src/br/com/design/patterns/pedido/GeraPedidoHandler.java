65fb9e8d18363b87e67f069590a0170f643f518d
Fernando <generatof@gmail.com>
2021-01-19 15:07:16 -0300

    Pattern Command Handler

--- src/br/com/design/patterns/pedido/GeraPedidoHandler.java
+++ src/br/com/design/patterns/pedido/GeraPedidoHandler.java
@@ -1,19 +1,27 @@
 package br.com.design.patterns.pedido;
 
 import br.com.design.patterns.orcamento.Orcamento;
+import br.com.design.patterns.pedido.acao.EnviarEmailPedido;
+import br.com.design.patterns.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.time.LocalDateTime;
 
 public class GeraPedidoHandler {
     //construtor com injecao de dependencias : repository, service, etc...
 
+
+
     public void execute(GeraPedido dados) {
         Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
 
         Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
 
-        System.out.println("Salvar pedido no Banco de Dados");
-        System.out.println("Enviar email com dados do novo pedido");
+        EnviarEmailPedido email = new EnviarEmailPedido();
+        SalvarPedidoNoBancoDeDados salvar = new SalvarPedidoNoBancoDeDados();
+
+        email.executar(pedido);
+        salvar.executar(pedido);
+
 
     }
 }
